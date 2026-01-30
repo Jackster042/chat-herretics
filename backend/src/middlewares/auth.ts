e3d04b6ef2e23819894ab3a02729396c8e2fa1f3
Nemanja Stojanovic <feemail042@gmail.com>
2026-01-30 13:46:44 +0100

    auth controllers /w clerk

---
+++ backend/src/middlewares/auth.ts
@@ -0,0 +1,26 @@
+import type { Request, Response, NextFunction } from "express";
+import {  requireAuth } from "@clerk/express";
+import { User } from "../models/User";
+import { getAuth } from "@clerk/express";
+
+export type AuthRequest = 
+Request & { userId?: string };
+
+export const protectedRoute = [
+    requireAuth(),
+    async (req:AuthRequest, res:Response, next:NextFunction) => {
+        try {
+            const { userId: clerkId } = getAuth(req);
+            const user = await User.findOne({ clerkId });
+            if(!user){
+                return res.status(401).json({ message: "Unauthorized" });
+            }
+            req.userId = user._id.toString();
+            next();
+        } catch (error) {
+            console.error("Error in protectedRoute middleware:", error);
+            res.status(500).json({ message: "Internal server error" });
+            next(error);
+        }
+    }
+]
--- backend/src/routes/auth-routes.ts
+++ backend/src/routes/auth-routes.ts
@@ -1,4 +1,10 @@
 import express from "express";
+import { getMe, authCallback } from "../controllers/auth-controller";
+import { protectedRoute } from "../middlewares/auth";
+
 const router = express.Router();
+
+router.get("/me",protectedRoute, getMe)
+router.post("/callback",  authCallback)
 
 export default router;
